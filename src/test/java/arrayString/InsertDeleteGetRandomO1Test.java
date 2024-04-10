2eb7142f112d6cc0d3a2a3ebb9ff14232d903303
Vadim Isaev <vadim.o.isaev@gmail.com>
2024-04-10 07:11:05 +0100

    smthing

--- src/test/java/arrayString/InsertDeleteGetRandomO1Test.java
+++ src/test/java/arrayString/InsertDeleteGetRandomO1Test.java
@@ -3,7 +3,9 @@ package arrayString;
 import static java.util.Arrays.asList;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -15,73 +17,74 @@ import arrayString.InsertDeleteGetRandomO1.RandomizedSet;
 
 public class InsertDeleteGetRandomO1Test {
 
+  final int getRandomTestRequestsCount = 1_000_000;
+  final double randomValuesProbabilityTreshold = 0.05d;// shouldn't be greater than 10% appearance of the random value
+                                                       // in comparison to average
+
   RandomizedSet subject = new RandomizedSet();
 
   @Test
   public void test() {
 
-    final double randomValuesProbabilityTreshold = 0.05d;// shouldn't be greater than 10% appearance of the random value
-                                                         // in comparison to average
-    final int getRandomTestRequestsCount = 1_000_000;
-
     Assertions.assertTrue(subject.insert(1));
-    testGetRandom(subject, getRandomTestRequestsCount, randomValuesProbabilityTreshold, asList(1));
+    testGetRandomExpectOnly(1);
 
     Assertions.assertFalse(subject.insert(1));
 
     Assertions.assertTrue(subject.insert(10));
-    testGetRandom(subject, getRandomTestRequestsCount, randomValuesProbabilityTreshold, asList(1, 10));
+    testGetRandomExpectOnly(1, 10);
 
     Assertions.assertFalse(subject.insert(1));
     Assertions.assertFalse(subject.insert(10));
 
     Assertions.assertTrue(subject.insert(20));
-    testGetRandom(subject, getRandomTestRequestsCount, randomValuesProbabilityTreshold, asList(1, 10, 20));
+    testGetRandomExpectOnly(1, 10, 20);
 
     Assertions.assertFalse(subject.insert(1));
     Assertions.assertFalse(subject.insert(10));
     Assertions.assertFalse(subject.insert(20));
 
     Assertions.assertTrue(subject.insert(30));
-    testGetRandom(subject, getRandomTestRequestsCount, randomValuesProbabilityTreshold, asList(1, 10, 20, 30));
+    testGetRandomExpectOnly(1, 10, 20, 30);
 
     Assertions.assertTrue(subject.remove(30));
-    testGetRandom(subject, getRandomTestRequestsCount, randomValuesProbabilityTreshold, asList(1, 10, 20));
+    testGetRandomExpectOnly(1, 10, 20);
     Assertions.assertFalse(subject.remove(30));
 
     Assertions.assertTrue(subject.remove(1));
-    testGetRandom(subject, getRandomTestRequestsCount, randomValuesProbabilityTreshold, asList(10, 20));
+    testGetRandomExpectOnly(10, 20);
     Assertions.assertFalse(subject.remove(30));
     Assertions.assertFalse(subject.remove(1));
 
     Assertions.assertTrue(subject.remove(20));
-    testGetRandom(subject, getRandomTestRequestsCount, randomValuesProbabilityTreshold, asList(10));
+    testGetRandomExpectOnly(10);
     Assertions.assertFalse(subject.remove(30));
     Assertions.assertFalse(subject.remove(1));
     Assertions.assertFalse(subject.remove(20));
   }
 
-  private static void testGetRandom(
-      final RandomizedSet set,
-      final int count,
-      final double thresholdRatio,
-      Collection<Integer> expectOnlyRandomsSorted) {
-    Map<Integer, Integer> stats = IntStream.range(0, count)
-        .map(c -> set.getRandom())
+  private void testGetRandomExpectOnly(int... expectOnlyRandoms) {
+    Map<Integer, Integer> stats = IntStream.range(0, getRandomTestRequestsCount)
+        .map(c -> subject.getRandom())
         .boxed()
         .collect(Collectors.groupingBy(c -> c, Collectors.reducing(0, e -> 1, Integer::sum)));
 
     int sum = stats.values().stream().mapToInt(Integer::intValue).sum();
 
-    Assertions.assertEquals(count, sum);
+    Assertions.assertEquals(getRandomTestRequestsCount, sum);
 
-    ArrayList<Integer> randoms = new ArrayList<>(stats.keySet());
-    randoms.sort(Integer::compareTo);
+    ArrayList<Integer> randomsSorted = new ArrayList<>(stats.keySet());
+    randomsSorted.sort(Integer::compareTo);
 
-    Assertions.assertIterableEquals(expectOnlyRandomsSorted, randoms);
+    List<Integer> expectOnlyRandomsSorted = IntStream.of(expectOnlyRandoms)
+        .boxed()
+        .sorted()
+        .collect(Collectors.toList());
 
-    final int avg = count / stats.size();
-    final double allowedDelta = avg * thresholdRatio;
+    Assertions.assertIterableEquals(expectOnlyRandomsSorted, randomsSorted);
+
+    final int avg = getRandomTestRequestsCount / stats.size();
+    final double allowedDelta = avg * randomValuesProbabilityTreshold;
     stats.values().forEach(cnt -> Assertions.assertEquals(avg, cnt, allowedDelta));
   }
 
