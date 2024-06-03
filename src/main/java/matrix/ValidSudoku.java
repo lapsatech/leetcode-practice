9243c46f7c5c929b44f593d18da8620a1977bb11
Vadim Isaev <vadim.o.isaev@gmail.com>
2024-06-03 11:22:37 +0100

    refact

--- src/main/java/matrix/ValidSudoku.java
+++ src/main/java/matrix/ValidSudoku.java
@@ -4,56 +4,46 @@ public class ValidSudoku {
 
   public boolean isValidSudoku(char[][] board) {
     for (int i1 = 0; i1 < 9; i1++) {
-      boolean[] byCols = new boolean[9];
-      boolean[] byRows = new boolean[9];
-      boolean[] bySquares = new boolean[9];
+      boolean[] tableByRows = new boolean[9];
+      boolean[] tableByCols = new boolean[9];
+      boolean[] tableBySubsquares = new boolean[9];
       for (int i2 = 0; i2 < 9; i2++) {
-
-        int d = idxOfRow(board, i1, i2);
-        if (d > 0) {
-          if (byRows[d - 1]) {
-            return false;
-          }
-          byRows[d - 1] = true;
+        if (!validate(tableByRows, valueOfRow(board, i1, i2))) {
+          return false;
         }
-
-        d = idxOfCol(board, i1, i2);
-        if (d > 0) {
-          if (byCols[d - 1]) {
-            return false;
-          }
-          byCols[d - 1] = true;
+        if (!validate(tableByCols, valueOfCol(board, i1, i2))) {
+          return false;
         }
-
-        d = idxOfSquare(board, i1, i2);
-        if (d > 0) {
-          if (bySquares[d - 1]) {
-            return false;
-          }
-          bySquares[d - 1] = true;
+        if (!validate(tableBySubsquares, valueOfSubsquare(board, i1, i2))) {
+          return false;
         }
       }
     }
     return true;
   }
 
-  private static int idxOfRow(char[][] board, int i1, int i2) {
-    return board[i1][i2] == '.'
-        ? -1
-        : board[i1][i2] - 48;
+  private static boolean validate(boolean[] memoTable, char value) {
+    if (value == '.') {
+      return true;
+    }
+    if (memoTable[value - 49]) {
+      return false;
+    }
+    memoTable[value - 49] = true;
+    return true;
   }
 
-  private static int idxOfCol(char[][] board, int i1, int i2) {
-    return board[i2][i1] == '.'
-        ? -1
-        : board[i2][i1] - 48;
+  private static char valueOfRow(char[][] board, int i1, int i2) {
+    return board[i1][i2];
   }
 
-  private static int idxOfSquare(char[][] board, int i1, int i2) {
-    int i = (((int) i1 / 3) * 3) + ((int) i2 / 3);
-    int j = (((int) i1 % 3) * 3) + ((int) i2 % 3);
-    return board[i][j] == '.'
-        ? -1
-        : board[i][j] - 48;
+  private static char valueOfCol(char[][] board, int i1, int i2) {
+    return board[i2][i1];
+  }
+
+  private static char valueOfSubsquare(char[][] board, int i1, int i2) {
+    int j1 = (((int) i1 / 3) * 3) + ((int) i2 / 3);
+    int j2 = (((int) i1 % 3) * 3) + ((int) i2 % 3);
+    return board[j1][j2];
   }
 }
